ced6ece7ca79c01bfce0b628019091bdd35ad3a9
John Victor-Faichney <john.victor.faichney@gmail.com>
2016-05-14 20:52:42 -0700

    detecting facial features

--- public/index.js
+++ public/index.js
@@ -112,7 +112,7 @@ document.getElementById('button-preview').onclick = function () {
       vid.id = "video"
       startTracker()
 
-    }, 2000)
+    }, 3000)
 
 };
 
@@ -127,28 +127,36 @@ function startTracker() {
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
-  var tracker = new tracking.ObjectTracker('face');
-  tracker.setInitialScale(4);
-  tracker.setStepSize(2);
-  tracker.setEdgesDensity(0.1);
+  // var tracker = new tracking.ObjectTracker('face');
+  var objects = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+    // var objects = new tracking.ObjectTracker([ 'eye', 'mouth']);
 
-  tracking.track('#video', tracker, { camera: true });
+  objects.setInitialScale(4);
+  objects.setStepSize(2);
+  objects.setEdgesDensity(0.1);
 
-  tracker.on('track', function(event) {
+  tracking.track('#video', objects);
+
+
+  objects.on('track', function(event) {
     context.clearRect(0, 0, canvas.width, canvas.height);
-
-    event.data.forEach(function(rect) {
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-    });
+    if (event.data.length === 0) {
+      // No objects were detected in this frame.
+    }
+    else {
+        event.data.forEach(function(rect) {
+          context.strokeStyle = '#a64ceb';
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          context.font = '11px Helvetica';
+          context.fillStyle = "#fff";
+          context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+          context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+        });
+      }
   });
 
   var gui = new dat.GUI();
-  gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
-  gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
-  gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+  gui.add(objects, 'edgesDensity', 0.1, 0.5).step(0.01);
+  gui.add(objects, 'initialScale', 1.0, 10.0).step(0.1);
+  gui.add(objects, 'stepSize', 1, 5).step(0.1);
 };
