a733fe51d5449c1d3634a4b5fbe5e8426479b3b6
John Victor-Faichney <john.victor.faichney@gmail.com>
2016-05-14 20:10:59 -0700

    proof of concept face tracking

--- public/index.js
+++ public/index.js
@@ -90,15 +90,65 @@ document.getElementById('button-preview').onclick = function () {
         function (mediaStream) {
             previewMedia.addStream(mediaStream);
             previewMedia.attach('#local-media');
+
+            // // Quick fix to get video working as-is
+            // var vid = document.getElementsByTagName("video")[0]
+            // vid.id = "video"
+            // vid.addEventListener('canplaythrough', function(e){
+            //   startTracker()
+            // })
+
         },
         function (error) {
             console.error('Unable to access local media', error);
             log('Unable to access Camera and Microphone');
         });
     };
+
+    // Start Tracking
+    setTimeout(function(){
+      // Quick fix to get video working as-is
+      var vid = document.getElementsByTagName("video")[0]
+      vid.id = "video"
+      startTracker()
+
+    }, 2000)
+
 };
 
 // Activity log
 function log(message) {
     document.getElementById('log-content').innerHTML = message;
 }
+
+// Tracking
+function startTracker() {
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+
+  var tracker = new tracking.ObjectTracker('face');
+  tracker.setInitialScale(4);
+  tracker.setStepSize(2);
+  tracker.setEdgesDensity(0.1);
+
+  tracking.track('#video', tracker, { camera: true });
+
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    event.data.forEach(function(rect) {
+      context.strokeStyle = '#a64ceb';
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
+
+  var gui = new dat.GUI();
+  gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
+  gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
+  gui.add(tracker, 'stepSize', 1, 5).step(0.1);
+};
